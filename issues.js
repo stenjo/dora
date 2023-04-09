edbdaeb646cf18f3d6b465e7fbd9f4fc79ede831
Sten Otto Johnsen <steoj@equinor.com>
2023-04-09 13:58:18 +0200

    initial issues

---
+++ issues.js
@@ -0,0 +1,18 @@
+// Issues.js
+import { Octokit, App } from "octokit";
+
+const octokit = new Octokit({
+    auth: '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'
+  });
+
+const {
+  data: { login },
+} = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
+    owner: 'stenjo',
+    repo: 'SbankenToYNAB',
+    headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
+})
+
+module.exports=issues
