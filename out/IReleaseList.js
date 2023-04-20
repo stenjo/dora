804b5a7b87403aee9e67e7024195cdc21ee16482
Sten Otto Johnsen <steoj@equinor.com>
2023-04-21 00:41:32 +0200

    wip

--- out/IReleaseList.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=IReleaseList.js.map
--- out/wait.js
+++
@@ -1,9 +0,0 @@
-export function wait(milliseconds) {
-    return new Promise((resolve) => {
-        if (typeof milliseconds !== 'number') {
-            throw new Error('milliseconds not a number');
-        }
-        setTimeout(() => resolve("done!"), milliseconds);
-    });
-}
-//# sourceMappingURL=wait.js.map
