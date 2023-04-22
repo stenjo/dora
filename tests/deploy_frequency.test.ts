5265568661241b8be64cf7df2ea455ba736908ee
Sten Otto Johnsen <steoj@equinor.com>
2023-04-22 22:41:12 +0200

    fix: clean up

--- tests/deploy_frequency.test.ts
+++ tests/deploy_frequency.test.ts
@@ -4,7 +4,7 @@ import fs from 'fs';
 import { ReleaseObj } from '../src/IReleaseList';
 
 describe('Deploy frequency should', () => {
-    // Release v0.0.1:2023-04-14
+    // Release v0.0.1:2023-04-14, v0.0.2:2023-04-22
     const releaselist:Array<ReleaseObj> = JSON.parse(fs.readFileSync('./tests/test-data/one-release.json', 'utf-8'));
     it('calculate releases pr week for release less than 1 week', () => {
         const df = new DeployFrequency(releaselist, "2023-04-14T22:33:11Z");
@@ -16,22 +16,22 @@ describe('Deploy frequency should', () => {
         const df = new DeployFrequency(releaselist, "2023-04-24T22:33:11Z");
         const wr = df.weekly();
 
-        expect(wr).toBe(0);
+        expect(wr).toBe(1);
     })
     it('montly calculate releases pr week for release more than 1 week ago', () => {
         const df = new DeployFrequency(releaselist, "2023-04-24T22:33:11Z");
         const mr = df.monthly();
 
-        expect(mr).toBe(1);
+        expect(mr).toBe(2);
     })
     it('calculate release rate last month', () => {
         const df = new DeployFrequency(releaselist, "2023-04-14T22:33:11Z");
         const rr = df.rate();
 
-        expect(rr).toBe('0.23');
+        expect(rr).toBe('0.47');
     })
     it('calculate release rate last month', () => {
-        const df = new DeployFrequency(releaselist, "2023-05-14T22:33:11Z");
+        const df = new DeployFrequency(releaselist, "2023-05-23T22:33:11Z");
         const rr = df.rate();
 
         expect(rr).toBe('0.00');
