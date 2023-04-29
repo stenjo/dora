b77df3f1c470216904c792eac2d0bbbdbd959e21
Sten Otto Johnsen <sten@thinea.no>
2023-04-29 14:54:44 +0200

    fix(issues): include all issues two months back (#28)
    Co-authored-by: Sten Otto Johnsen <steoj@equinor.com>

--- src/IssuesList.ts
+++ src/IssuesList.ts
@@ -5,17 +5,21 @@ import * as core from '@actions/core';
 export class IssuesList {
   
   async issueList(token: string|undefined, owner: string, repo: string): Promise<any> {
+    const today = new Date();
+    const since = new Date(today.valueOf() - (61*24*60*60*1000))  // Go two months back
     try {
       const octokit = new Octokit({
         auth: token
       });
   
-      const result = await octokit.request('GET /repos/{owner}/{repo}/issues', {
+      const result = await octokit.request('GET /repos/{owner}/{repo}/issues?state=all&since={since}', {
         owner,
         repo,
         headers: {
             'X-GitHub-Api-Version': '2022-11-28'
-        }
+        },
+        since: since.toISOString(),
+
       });
   
       return Promise.resolve(result.data);
