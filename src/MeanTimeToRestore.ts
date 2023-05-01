0841fb5827525b84143fffbeed5d4640507c5adb
Sten Otto Johnsen <steoj@equinor.com>
2023-05-01 11:46:29 +0200

    r

--- src/MeanTimeToRestore.ts
+++ src/MeanTimeToRestore.ts
@@ -1,71 +1,74 @@
 import { IssueObject } from "./IIssue";
 import { ReleaseObject } from "./IReleaseList";
 
-export interface BugTimes
-{
-    start: string;
-    end: string;
+export interface BugTimes {
+  start: string;
+  end: string;
 }
 
 export class MeanTimeToRestore {
-    today: Date;
-    issues: IssueObject[];
-    releases: ReleaseObject[];
+  today: Date;
+  issues: IssueObject[];
+  releases: ReleaseObject[];
 
-    constructor(issues: IssueObject[], releases: ReleaseObject[], today: Date | null = null) {
-        if (today === null) {
-            this.today = new Date();
-          } else {
-            this.today = today;
+  constructor(
+    issues: IssueObject[],
+    releases: ReleaseObject[],
+    today: Date | null = null
+  ) {
+    if (today === null) {
+      this.today = new Date();
+    } else {
+      this.today = today;
+    }
+    this.issues = issues;
+    this.releases = releases;
+  }
+
+  getTimeDiff(bugTime: BugTimes): number {
+    const startTime = +new Date(bugTime.start);
+    const endTime = +new Date(bugTime.end);
+
+    return (endTime - startTime) / (1000 * 60 * 60 * 24);
+  }
+
+  getBugCount(): Array<BugTimes> {
+    const filters = {
+      labeledBug: function (item: IssueObject, today: number): boolean {
+        let found = false;
+        item.labels.forEach(function (label) {
+          if (label.name === "bug") {
+            found = true;
           }
-          this.issues = issues;  
-          this.releases = releases;
-    }
+        });
+        const d = new Date(item.created_at);
+        return found && d.getTime() > today - 30 * 24 * 60 * 60 * 1000;
+      },
+    };
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const jsonQuery = require("json-query");
+    const bugs = jsonQuery("[*:labeledBug(" + this.today.getTime() + ")]", {
+      data: this.issues,
+      locals: filters,
+    }).value;
 
-    getTimeDiff(start: string, end: string): number {
-        const startTime = +(new Date(start));
-        const endTime = +(new Date(end));
+    // eslint-disable-next-line prefer-const
+    let values: Array<BugTimes> = [];
+    bugs.forEach(function (element: IssueObject) {
+      if (element.closed_at !== null) {
+        values.push({ start: element.created_at, end: element.closed_at });
+      }
+    }, this);
 
-        return (endTime - startTime) / (1000*60*60*24);
-    }
+    return values;
+  }
+  getReleaseTimes(): string[] {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const jsonQuery = require("json-query");
+    const dates = jsonQuery("[*].published_at", {
+      data: this.releases,
+    }).value;
 
-    getBugCount(): Array<{start:string, end:string}> {
-        const filters = {
-            labeledBug: function (item: IssueObject, today: number): boolean {
-                let found = false;
-                item.labels.forEach(function (label) {
-                    if (label.name === "bug") {
-                        found = true;
-                    }
-                });
-                const d = new Date(item.created_at);
-                return found && d.valueOf() > today - (30*24*60*60*1000);
-            }
-        }
-            // eslint-disable-next-line @typescript-eslint/no-var-requires
-        const jsonQuery = require("json-query");
-        const bugs = jsonQuery("[*:labeledBug(" + this.today.valueOf() + ")]", {
-        data: this.issues,
-        locals: filters,
-        }).value;
-
-        // eslint-disable-next-line prefer-const
-        let values:Array<{start:string, end:string}> = [];
-        bugs.forEach(function (element: IssueObject) {
-            if(element.closed_at !== null) {
-                values.push({start: element.created_at, end: element.closed_at});
-            }
-        }, this );
-
-        return values;
-    }
-    getReleaseTimes(): string[] {
-        // eslint-disable-next-line @typescript-eslint/no-var-requires
-        const jsonQuery = require("json-query");
-        const dates = jsonQuery("[*].published_at", {
-            data: this.releases,
-        }).value;
-
-        return dates;
-    }
+    return dates;
+  }
 }
--- tests/MeanTimeToRestore.test.ts
+++ tests/MeanTimeToRestore.test.ts
@@ -1,30 +1,42 @@
 import { IssueObject } from "../src/IIssue";
 import { ReleaseObject } from "../src/IReleaseList";
 import fs from "fs";
-import { MeanTimeToRestore } from "../src/MeanTimeToRestore";
+import { BugTimes, MeanTimeToRestore } from "../src/MeanTimeToRestore";
 
-describe("MeanTimeToRestore should", ()=> {
-    const issues: IssueObject[] = JSON.parse(fs.readFileSync('./tests/test-data/issue-list.json'));
-    const releases: ReleaseObject[] = JSON.parse(fs.readFileSync('./tests/test-data/releases.json'))
-    const mttr = new MeanTimeToRestore(issues, releases, new Date("2023-04-30T16:50:53Z"));
+describe("MeanTimeToRestore should", () => {
+  const issues: IssueObject[] = JSON.parse(
+    fs.readFileSync("./tests/test-data/issue-list.json")
+  );
+  const releases: ReleaseObject[] = JSON.parse(
+    fs.readFileSync("./tests/test-data/releases.json")
+  );
+  const mttr = new MeanTimeToRestore(
+    issues,
+    releases,
+    new Date("2023-04-30T16:50:53Z")
+  );
 
-    it("get bugs last month", ()=> {
-        const bugCount = mttr.getBugCount();
+  it("get bugs last month", () => {
+    const bugCount = mttr.getBugCount();
 
-        expect(bugCount.length).toBe(2);
-        expect(bugCount[0].start).toBe("2023-04-25T21:21:49Z");
-        expect(bugCount[1].end).toBe("2023-04-23T16:47:40Z");
-    })
+    expect(bugCount.length).toBe(2);
+    expect(bugCount[0].start).toBe("2023-04-25T21:21:49Z");
+    expect(bugCount[1].end).toBe("2023-04-23T16:47:40Z");
+  });
 
-    it("get release times", () => {
-        const releaseTimes: string[] = mttr.getReleaseTimes();
+  it("get release times", () => {
+    const releaseTimes: string[] = mttr.getReleaseTimes();
 
-        expect(releaseTimes[0]).toEqual("2023-04-30T17:29:44Z");
-    })
-    it("calculate time for a bug", ()=>{
+    expect(releaseTimes[0]).toEqual("2023-04-30T17:29:44Z");
+  });
+  
+  it("calculate time for a bug", () => {
+    const bugTime: BugTimes = {
+      start: "2023-04-25T21:21:49Z",
+      end: "2023-04-26T21:21:49Z",
+    };
+    const restoreTime: number = mttr.getTimeDiff(bugTime);
 
-        const restoreTime: number = mttr.getTimeDiff("2023-04-25T21:21:49Z", "2023-04-26T21:21:49Z");
-
-        expect(restoreTime).toBe(1);
-    })
-})
+    expect(restoreTime).toBe(1);
+  });
+});
