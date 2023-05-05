3b5e08ea5e6b8d310ce3393e6ed6852d86afdd99
Sten Johnsen <sten@thinea.no>
2023-05-05 10:46:48 +0200

    update with releases

--- src/LeadTime.ts
+++ src/LeadTime.ts
@@ -1,14 +1,17 @@
 import { CommitObject } from "./ICommitsList";
 import { PullRequestObject } from "./IPullRequest";
+import { ReleaseObject } from "./IReleaseList";
 
 const ONE_DAY = 24 * 60 * 60 * 1000;
 export class LeadTime {
   pulls: PullRequestObject[];
+  releases: number[];
   today: Date;
   getCommits: (pullNo: number) => Promise<CommitObject[]>;
 
   constructor(
     pulls: PullRequestObject[],
+    releases: ReleaseObject[],
     getCommits: (pullNo: number) => Promise<CommitObject[]>,
     today: Date | null = null
   ) {
@@ -19,11 +22,12 @@ export class LeadTime {
     }
 
     this.pulls = pulls.filter((p)=>+new Date(p.merged_at) > this.today.valueOf() - 31 * ONE_DAY);
+    this.releases = releases.map((r)=>+new Date(r.published_at));
     this.getCommits = getCommits;
   }
 
   async getLeadTime(): Promise<number> {
-    if (this.pulls.length === 0) {
+    if (this.pulls.length === 0 || this.releases.length === 0) {
       return 0;
     }
     const leadTimes: number[] = [];
@@ -35,17 +39,18 @@ export class LeadTime {
         pull.base.ref === "main"
       ) {
         const mergeTime = +new Date(pull.merged_at);
+        const deployTime = this.releases.filter((r)=>r > mergeTime)[0];
         const commmmits = await this.getCommits(pull.number);
         const commitTime = commmmits
           .map((c) => +new Date(c.commit.committer.date))
           .sort()[0];
-        leadTimes.push((mergeTime - commitTime) / ONE_DAY);
+        leadTimes.push((deployTime - commitTime) / ONE_DAY);
       }
     }
     if (leadTimes.length === 0) {
         return 0;
     }
 
-    return leadTimes.reduce((p, c) => p + c) / leadTimes.length;
+    return Math.round(leadTimes.reduce((p, c) => p + c) / leadTimes.length * 100) / 100;
   }
 }
