580649678c97effd142d2d006e8d841a33044565
Sten Johnsen <sten@thinea.no>
2023-05-21 13:06:59 +0200

    fixed test report

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
   testMatch: ['**/*.test.ts'],
+  coverageReporters: ['json', 'text'],
   transform: {
     '^.+\\.ts$': 'ts-jest'
   },
