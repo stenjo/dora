d628f8a87814c8722e8c3351eab8fd8d95e5dff2
Sten Johnsen <sten@thinea.no>
2024-08-28 19:57:30 +0200

    wip

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  clearMocks: true,
-  moduleFileExtensions: ['js', 'ts'],
-  testMatch: ['**/*.test.ts'],
-  coverageReporters: ['lcov','json-summary', 'text', 'json'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest'
-  },
-};
